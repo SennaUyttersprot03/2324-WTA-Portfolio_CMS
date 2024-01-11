50515fc3a9dcb3f79dffbc8909c4d94587d88462
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-11 14:49:51 +0100

    refactor api calls into controllers

---
+++ controllers/CommentController.tsx
@@ -0,0 +1,36 @@
+import { FreshContext } from "$fresh/server.ts";
+import Post from "../types/Post.tsx";
+import { validateComment } from "../validators/CommentValidator.tsx";
+import { hasErrors } from "../validators/HelperFunctions.tsx";
+
+const kv = await Deno.openKv();
+
+const addComment = async (req: Request, ctx: FreshContext) => {
+  const id = ctx.params.id;
+  const body = await req.json();
+  const errors = validateComment(body);
+
+  if (hasErrors(errors)) {
+    return new Response(
+      JSON.stringify({ message: "The given data was invalid", errors }),
+      { status: 422 },
+    );
+  }
+
+  const record = await kv.get(["post", Number.parseInt(id)]);
+  const post = record.value as Post;
+  const comments = post.comments || [] as Comment[];
+
+  comments.push({ ...body, createdAt: new Date() });
+  post.comments = comments;
+
+  const postKey = ["post", post.id];
+  await kv.set(postKey, post);
+
+  return new Response(
+    JSON.stringify({ message: "Comment successfully added", post }),
+    { status: 201 },
+  );
+};
+
+export { addComment };
--- routes/api/posts/[id]/index.tsx
+++ routes/api/posts/[id]/index.tsx
@@ -1,27 +1,16 @@
 import { Handlers } from "$fresh/server.ts";
+import {
+  deletePostById,
+  postById,
+} from "../../../../controllers/PostController.tsx";
 import Post from "../../../../types/Post.tsx";
-
-const kv = await Deno.openKv();
 
 export const handler: Handlers<Post> = {
   async GET(_req, ctx) {
-    const id = ctx.params.id;
-    const record = await kv.get(["post", Number.parseInt(id)]);
-    const post = record.value as Post;
-
-    if (!post) {
-      return new Response(JSON.stringify({ message: "Post not found" }), {
-        status: 404,
-      });
-    }
-    return new Response(JSON.stringify({ message: "Here is your post", post }));
+    return await postById(_req, ctx);
   },
 
   async DELETE(_req, ctx) {
-    const id = ctx.params.id;
-    await kv.delete(["post", Number.parseInt(id)]);
-    return new Response(
-      JSON.stringify({ message: "Post successfully deleted" }),
-    );
+    return await deletePostById(_req, ctx);
   },
 };
