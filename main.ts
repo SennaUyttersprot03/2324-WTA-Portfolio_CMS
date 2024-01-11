77362bc0d638740ae1522f75e4141d4d4db1c774
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-11 20:00:23 +0100

    env fix

--- main.ts
+++ main.ts
@@ -5,12 +5,9 @@
 /// <reference lib="deno.ns" />
 
 import "$std/dotenv/load.ts";
-import { config as dotEnvConf } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-
-dotEnvConf({ allowEmptyValues: true });
 
 await start(manifest, config);
