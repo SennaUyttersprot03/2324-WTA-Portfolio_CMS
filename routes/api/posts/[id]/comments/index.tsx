5135ee9af2c477a4e3f2637af00c2ed7ce710c1f
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-18 11:59:47 +0100

    api removed from CMS

--- routes/api/posts/[id]/comments/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Handlers } from "$fresh/server.ts";
-import { addComment } from "../../../../../controllers/CommentController.tsx";
-import Post from "../../../../../types/Post.tsx";
-
-export const handler: Handlers<Post> = {
-  async POST(req, ctx) {
-    return await addComment(req, ctx);
-  },
-};
